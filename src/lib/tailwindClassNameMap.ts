d9ec356b6347c64e09f1aed494a90e509ea51c87
Tynahan <nusilite@gmail.com>
2025-10-16 06:32:48 +0900

    ✨ Feat(common layout): Container 제작

--- src/lib/tailwindClassNameMap.ts
+++ src/lib/tailwindClassNameMap.ts
@@ -1,4 +1,4 @@
-const spacingMap = {
+export const gapMap = {
   none: '',
   xs: 'gap-1',
   sm: 'gap-2',
@@ -8,4 +8,36 @@ const spacingMap = {
   xxl: 'gap-8',
 }
 
-export const gapMap = { ...spacingMap }
+export const paddingMap = {
+  none: '',
+  xs: 'p-1',
+  sm: 'p-2',
+  md: 'p-3',
+  lg: 'p-4',
+  xl: 'p-6',
+  xxl: 'p-8',
+}
+export const paddingTopMap = {
+  none: '',
+  xs: 'pt-1',
+  sm: 'pt-2',
+  md: 'pt-3',
+  lg: 'pt-4',
+  xl: 'pt-6',
+  xxl: 'pt-8',
+}
+export const paddingBottomMap = {
+  none: '',
+  xs: 'pb-1',
+  sm: 'pb-2',
+  md: 'pb-3',
+  lg: 'pb-4',
+  xl: 'pb-6',
+  xxl: 'pb-8',
+}
+
+export const widthMap = {
+  sm: 'w-2xl', // 672px 좁은 모달
+  md: 'w-4xl', // 896px 넓은 모달 | 공고 세부 페이지
+  lg: 'w-7xl', // 1280px 그 외 넓은 페이지
+}
