c703c58d4da1dc7bb9ee9d502ec637e405432d09
Tynahan <nusilite@gmail.com>
2025-10-16 05:21:43 +0900

    ✨ Feat(common layout): GridContainer 제작

--- src/components/commonInGeneral/layout/_GridContainer.tsx
+++ src/components/commonInGeneral/layout/_GridContainer.tsx
@@ -1,46 +1,22 @@
-import type {
-  Style,
-  Auto,
-  None,
-  XsToXl,
-  DivProps,
-} from '../../packageInterfaces'
-import styles from './_GridContainer.module.css'
+import { gapMap } from '../../../lib/tailwindClassNameMap'
+import type { DivProps, None, XsToXxl } from '../../../types'
 
 interface WithGridContainerProps {
-  gap?: XsToXl | None
-  cols?: number | Auto
-  colMinWidth?: XsToXl
-  rows?: number
+  gap?: XsToXxl | None
 }
 
 const GridContainer = ({
   gap = 'md',
-  cols,
-  colMinWidth,
-  rows,
   ...props
 }: DivProps & WithGridContainerProps) => {
-  const { style, className, children, ...rest } = props
-  const styleForVar: Style = {}
+  const { className, children, ...rest } = props
 
-  styleForVar['--grid-cols'] =
-    cols === 'auto'
-      ? `repeat(auto-fill, minmax(var(--sizing-${colMinWidth}), 1fr))`
-      : `repeat(${cols}, 1fr)`
-
-  styleForVar['--gap'] = gap === 'none' ? 0 : `var(--spacing-${gap})`
-
-  if (rows) {
-    styleForVar['--grid-rows'] = `repeat(${rows})`
-    styleForVar['--height'] = '100%'
-  }
+  const gapResult = gapMap[gap] ?? ''
 
   return (
     <div
       {...rest}
-      style={{ ...styleForVar, ...style }}
-      className={`${styles.grid} ${className}`}
+      className={`${className} ${gapResult} grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))]`}
     >
       {children}
     </div>
--- src/testRoutes/testPages/thepott/ThepottTestPage.tsx
+++ src/testRoutes/testPages/thepott/ThepottTestPage.tsx
@@ -1,5 +1,49 @@
+import {
+  GridContainer,
+  Vstack,
+} from '../../../components/commonInGeneral/layout'
+
+const Box = () => {
+  return <div className="h-[100px] w-[100px] bg-amber-300">뭔가 있음</div>
+}
+
 const ThepottTestPage = () => {
-  return <div>ThePott Test Page</div>
+  return (
+    <Vstack className="w-full bg-rose-200">
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <GridContainer>
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </GridContainer>
+    </Vstack>
+  )
 }
 
 export default ThepottTestPage
