e2650802ec84a6a0511d36ad0bf019d7a2fe920d
Tynahan <nusilite@gmail.com>
2025-10-16 06:59:16 +0900

    📝 Chore: 작업 안 된 RoundBox, Text 코멘트 처리

--- src/components/commonInGeneral/text/Text.tsx
+++ src/components/commonInGeneral/text/Text.tsx
@@ -1,37 +1,37 @@
-import styles from './Text.module.css'
-import type { PProps, Style, XsToXl } from '../../packageInterfaces'
-
-interface TextProps {
-  isMuted?: boolean
-  isBold?: boolean
-  size?: XsToXl
-}
-const Text = ({
-  isMuted,
-  isBold,
-  size = 'md',
-  ...props
-}: TextProps & PProps) => {
-  const { children, style, className, ...rest } = props
-
-  const styleForVar: Style = {}
-  if (isMuted) {
-    styleForVar['--color'] = 'var(--color-muted)'
-  }
-  if (isBold) {
-    styleForVar['--font-weight'] = 'var(--font-weight-semibold)'
-  }
-  styleForVar['--font-size'] = `var(--text-${size})`
-
-  return (
-    <p
-      {...rest}
-      style={{ ...styleForVar, ...style }}
-      className={`${className} ${styles.text}`}
-    >
-      {children}
-    </p>
-  )
-}
-
-export default Text
+// import styles from './Text.module.css'
+// import type { PProps, Style, XsToXl } from '../../packageInterfaces'
+//
+// interface TextProps {
+//   isMuted?: boolean
+//   isBold?: boolean
+//   size?: XsToXl
+// }
+// const Text = ({
+//   isMuted,
+//   isBold,
+//   size = 'md',
+//   ...props
+// }: TextProps & PProps) => {
+//   const { children, style, className, ...rest } = props
+//
+//   const styleForVar: Style = {}
+//   if (isMuted) {
+//     styleForVar['--color'] = 'var(--color-muted)'
+//   }
+//   if (isBold) {
+//     styleForVar['--font-weight'] = 'var(--font-weight-semibold)'
+//   }
+//   styleForVar['--font-size'] = `var(--text-${size})`
+//
+//   return (
+//     <p
+//       {...rest}
+//       style={{ ...styleForVar, ...style }}
+//       className={`${className} ${styles.text}`}
+//     >
+//       {children}
+//     </p>
+//   )
+// }
+//
+// export default Text
