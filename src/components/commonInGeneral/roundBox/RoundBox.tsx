6b4d0f294232b86ad627ef21e9b4fd3468b8486c
Tynahan <nusilite@gmail.com>
2025-10-16 06:59:16 +0900

    📝 Chore: 작업 안 된 RoundBox, Text 코멘트 처리

--- src/components/commonInGeneral/roundBox/RoundBox.tsx
+++ src/components/commonInGeneral/roundBox/RoundBox.tsx
@@ -1,37 +1,37 @@
-import type { DivProps, None, Style, XsToXl } from '../../packageInterfaces'
-import styles from './RoundBox.module.css'
-
-interface WithRoundBoxProps {
-  isShadowed?: boolean
-  padding?: XsToXl | None
-}
-
-const RoundBox = ({
-  isShadowed = true,
-  padding = 'md',
-  ...props
-}: DivProps & WithRoundBoxProps) => {
-  const { style, className, children, ...rest } = props
-
-  const styleForVar: Style = {}
-  styleForVar['--shadow'] = isShadowed ? 'var(--drop-shadow-md)' : 0
-  styleForVar['--padding'] =
-    padding === 'none' ? 0 : `var(--spacing-${padding})`
-  // styleForVar["--padding"] = "var(--spacing-lg)"
-  // styleForVar["--border"] = isShadowed ? "none" : "1px solid var(--color-dimdimdim)"
-  styleForVar['--border-color-hover'] = isShadowed
-    ? 'transparent'
-    : 'var(--color-dimdim)'
-
-  return (
-    <div
-      {...rest}
-      style={{ ...styleForVar, ...style }}
-      className={`${className} ${styles.roundBox}`}
-    >
-      {children}
-    </div>
-  )
-}
-
-export default RoundBox
+// import type { DivProps, None, Style, XsToXl } from '../../packageInterfaces'
+// import styles from './RoundBox.module.css'
+//
+// interface WithRoundBoxProps {
+//   isShadowed?: boolean
+//   padding?: XsToXl | None
+// }
+//
+// const RoundBox = ({
+//   isShadowed = true,
+//   padding = 'md',
+//   ...props
+// }: DivProps & WithRoundBoxProps) => {
+//   const { style, className, children, ...rest } = props
+//
+//   const styleForVar: Style = {}
+//   styleForVar['--shadow'] = isShadowed ? 'var(--drop-shadow-md)' : 0
+//   styleForVar['--padding'] =
+//     padding === 'none' ? 0 : `var(--spacing-${padding})`
+//   // styleForVar["--padding"] = "var(--spacing-lg)"
+//   // styleForVar["--border"] = isShadowed ? "none" : "1px solid var(--color-dimdimdim)"
+//   styleForVar['--border-color-hover'] = isShadowed
+//     ? 'transparent'
+//     : 'var(--color-dimdim)'
+//
+//   return (
+//     <div
+//       {...rest}
+//       style={{ ...styleForVar, ...style }}
+//       className={`${className} ${styles.roundBox}`}
+//     >
+//       {children}
+//     </div>
+//   )
+// }
+//
+// export default RoundBox
